11febcddd09fca7e698dfde91d592e5fd9f1ceb5
rryuuni <fkdbs1122@naver.com>
2025-08-10 01:26:30 +0900

    Feat/#6 : 동행그룹 전체조회 완료

--- ie_um/src/pages/Cooperate/Cooperate.jsx
+++ ie_um/src/pages/Cooperate/Cooperate.jsx
@@ -1,5 +1,32 @@
-const Cooperate = () => {
-   return <div>Cooperate</div>;
+import * as S from './CooperateStyle';
+import { DummyCooperate } from '../../constants/DummyData';
+
+const Cooperate = ({ cooperate = DummyCooperate }) => {
+   return (
+      <S.Wrap>
+         <S.Title>동행그룹</S.Title>
+         <S.CreateBtn>그룹생성</S.CreateBtn>
+         {cooperate.map((item) => (
+            <S.Card>
+               <div key={item.id}>
+                  <S.CardWrap>
+                     <div>
+                        <S.CardTitle>{item.title}</S.CardTitle>
+                        <S.CardDate>{item.date}</S.CardDate>
+                        <S.CardPlace>{item.place}</S.CardPlace>
+                     </div>
+                     <S.BtnWrap>
+                        <S.CardCapacity>
+                           {item.people} / {item.capacity}
+                        </S.CardCapacity>
+                        <S.CardBtn>신청하기</S.CardBtn>
+                     </S.BtnWrap>
+                  </S.CardWrap>
+               </div>
+            </S.Card>
+         ))}
+      </S.Wrap>
+   );
 };
 
 export default Cooperate;
---
+++ ie_um/src/pages/Cooperate/CooperateStyle.jsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+   display: flex;
+   align-items: center;
+   flex-direction: column;
+   padding: 10px 30px;
+`;
+
+export const Title = styled.div`
+   font-size: 25px;
+   font-weight: 500;
+`;
+
+export const CreateBtn = styled.button`
+   background-color: ${(props) => props.theme.mainColor};
+   color: white;
+   width: 68px;
+   height: 30px;
+   border-radius: 10px;
+   font-weight: 600;
+   margin-top: -4px;
+   margin-bottom: 8px;
+   margin-left: auto;
+   margin-right: 3px;
+`;
+
+export const CardWrap = styled.div`
+   display: flex;
+   flex-direction: row;
+   justify-content: space-between;
+`;
+
+export const Card = styled.div`
+   display: flex;
+   flex-direction: column;
+   box-shadow: 0 0 3px 1px rgb(0, 0, 0, 0.12);
+   border-radius: 10px;
+   width: 100%;
+   height: 118px;
+   margin-bottom: 20px;
+   padding: 25px;
+`;
+
+export const CardTitle = styled.div`
+   font-weight: 700;
+   font-size: 20px;
+`;
+
+export const CardDate = styled.div`
+   font-weight: 400;
+   font-size: 13px;
+   padding-top: 15px;
+`;
+
+export const CardPlace = styled.div`
+   font-weight: 500;
+   font-size: 13px;
+   color: ${(props) => props.theme.mainColor};
+   padding-top: 5px;
+`;
+
+export const CardCapacity = styled.div`
+   font-weight: 400;
+   font-size: 10px;
+`;
+
+export const CardBtn = styled.button`
+   width: 62px;
+   height: 26px;
+   border-radius: 10px;
+   margin-top: 3px;
+   color: white;
+   font-size: 12px;
+   font-weight: 600;
+   background-color: ${(props) => props.theme.mainColorLight};
+`;
+
+export const BtnWrap = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   margin-top: auto;
+   margin-bottom: -5px;
+`;
