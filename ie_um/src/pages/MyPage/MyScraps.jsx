f0cd243444e404326f27d2f696d09f327fb78883
dz-1010 <qwop_01@naver.com>
2025-08-13 21:47:40 +0900

    Feat/#7: 저장한 장소 모달 추가

--- ie_um/src/pages/MyPage/MyScraps.jsx
+++ ie_um/src/pages/MyPage/MyScraps.jsx
@@ -1,11 +1,13 @@
 import { RiBookmarkFill, RiMore2Fill } from 'react-icons/ri';
 import * as S from './Style/MyScrapsStyle';
-
 import { useState } from 'react';
 import MyPagination from '../../components/Pagination';
+import useModal from '../../hooks/useModal';
+import DeleteModal from '../../components/Modal/DeleteModal';
 
 const MyScrapsLocation = () => {
    const [activePage, setActivePage] = useState(1);
+   const { openModal, closeModal } = useModal();
 
    const MOCK = Array.from({ length: 20 }).map((_, i) => ({
       id: i + 1,
@@ -25,6 +27,7 @@ const MyScrapsLocation = () => {
    return (
       <S.Container>
          <S.Title>저장한 장소</S.Title>
+         <DeleteModal onclose={() => closeModal('Delete_modal')} />
          {currentItems.map((item) => (
             <S.Item key={item.id}>
                <S.IconWrap>
@@ -36,7 +39,7 @@ const MyScrapsLocation = () => {
                   <S.Address>{item.address}</S.Address>
                </S.TextWrap>
 
-               <S.MoreButton>
+               <S.MoreButton onClick={() => openModal('Delete_modal')}>
                   <RiMore2Fill size={20} color="#555" />
                </S.MoreButton>
             </S.Item>
